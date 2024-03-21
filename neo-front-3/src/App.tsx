2b0e1c31a776891ee0c476f4594e09016e1272a9
lopesmfelipe <felipemlopes0009@gmail.com>
2024-03-20 22:34:42 -0600

    NEO V3

---
+++ neo-front-3/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="image-container">
+        <p>Hello world</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
